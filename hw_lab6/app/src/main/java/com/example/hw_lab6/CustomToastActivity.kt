aafdff3f1b30530ed29116cedac14fe1327bd785
HongMingE <t110360150@ntut.org.tw>
2023-11-08 23:47:31 +0800

    hw2_ch456

---
+++ hw_lab6/app/src/main/java/com/example/hw_lab6/CustomToastActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hw_lab6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomToastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.custom_toast)
+    }
+}
